7aeca86617a85ad1fbd550a6aa02be859fbf875b
oliverokeefe <62572348+oliverokeefe@users.noreply.github.com>
2020-06-04 15:45:11 -0500

    main.ts
    Started coding of main.ts

--- Project-MurderDome/client/public/javascripts/client/src/scripts/main.js
+++ Project-MurderDome/client/public/javascripts/client/src/scripts/main.js
@@ -1,10 +1,46 @@
+let _data = {
+    name: undefined,
+    nameInput: undefined,
+    stats: {
+        str: undefined,
+        dex: undefined,
+        con: undefined,
+        int: undefined,
+        wis: undefined,
+        cha: undefined
+    },
+    statInputs: {
+        str: undefined,
+        dex: undefined,
+        con: undefined,
+        int: undefined,
+        wis: undefined,
+        cha: undefined
+    },
+    statInputHandler: function (stat) {
+        _data.stats[stat] = +_data.statInputs[stat].value;
+        console.log(_data);
+        return;
+    }
+};
 function populateDOMElementVariables() {
+    _data.nameInput = document.getElementById("nameInput");
+    Object.keys(_data.statInputs).forEach(function (stat) {
+        _data.statInputs[stat] = document.getElementById(`${stat}Input`);
+    });
     return;
 }
 function addHandlers() {
+    Object.keys(_data.statInputs).forEach(function (stat) {
+        _data.statInputs[stat].addEventListener('change', () => { _data.statInputHandler(stat); });
+    });
     return;
 }
 function init() {
+    populateDOMElementVariables();
+    addHandlers();
+    console.log("initialized");
+    console.log(_data);
     return;
 }
 window.onload = function () {
--- Project-MurderDome/client/src/scripts/main.ts
+++ Project-MurderDome/client/src/scripts/main.ts
@@ -1,7 +1,50 @@
 
+import type { stats, statKey } from '../../../shared/src/types/types';
+
+
+type dataModel = {
+    name: string,
+    nameInput: HTMLInputElement,
+    stats: stats,
+    statInputs: { [stat in statKey]: HTMLInputElement }
+    statInputHandler: Function
+};
+
+let _data: dataModel = {
+    name: undefined,
+    nameInput: undefined,
+    stats: {
+        str: undefined,
+        dex: undefined,
+        con: undefined,
+        int: undefined,
+        wis: undefined,
+        cha: undefined
+    },
+    statInputs: {
+        str: undefined,
+        dex: undefined,
+        con: undefined,
+        int: undefined,
+        wis: undefined,
+        cha: undefined
+    },
+    statInputHandler: function (stat: statKey) {
+        _data.stats[stat] = +_data.statInputs[stat].value;
+        console.log(_data);
+        return;
+    }
+};
+
+
 
 function populateDOMElementVariables() {
 
+    _data.nameInput = document.getElementById("nameInput") as HTMLInputElement;
+
+    Object.keys(_data.statInputs).forEach(function (stat: statKey) {
+        _data.statInputs[stat] = document.getElementById(`${stat}Input`) as HTMLInputElement;
+    });
 
     return;
 }
@@ -9,12 +52,22 @@ function populateDOMElementVariables() {
 function addHandlers() {
 
 
+
+    Object.keys(_data.statInputs).forEach(function (stat: statKey) {
+        _data.statInputs[stat].addEventListener('change', () => { _data.statInputHandler(stat) });
+    });
+
     return;
 }
 
 
 function init() {
 
+    populateDOMElementVariables();
+    addHandlers();
+
+    console.log("initialized");
+    console.log(_data);
     return;
 }
 
